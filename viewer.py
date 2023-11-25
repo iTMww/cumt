a40d3a3dcd53f92d744a63528fac7243eb4e69c9
iTMww <zkw1625090649@hotmail.com>
2023-11-25 16:12:36 +0800

    1

--- viewer.py
+++ viewer.py
@@ -9,8 +9,9 @@ PATH = Path(__file__).parent / 'resource'
 
 class bianjiqi(ttk.Frame):
 
-    def __init__(self,):
-        super().__init__
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.pack(fill=BOTH, expand=YES)
 
         # 图片
         self.images = [
@@ -33,7 +34,7 @@ class bianjiqi(ttk.Frame):
 
         # 表头
         top_frame = ttk.Frame(self, padding=5, style=SECONDARY)
-        top_frame,grid(row=0, column=0, columnspan=5, sticky=EW)
+        top_frame, grid(row=0, column=0, columnspan=5, sticky=EW)
 
         top_label = ttk.Label(
             master=top_frame,
@@ -68,7 +69,7 @@ class bianjiqi(ttk.Frame):
 if __name__ == '__main__':
 
     app = ttk.Window("PC Cleaner", "pulse")
-    bianjiqi()
+    bianjiqi(app)
     app.mainloop()
 
 
