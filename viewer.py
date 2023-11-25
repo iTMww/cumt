9ef68ab5c4ea2790841055d2a1ee3a16c928743c
iTMww <zkw1625090649@hotmail.com>
2023-11-25 19:36:36 +0800

    .

--- viewer.py
+++ viewer.py
@@ -1,6 +1,8 @@
 import ttkbootstrap as ttk
+from ttkbootstrap.dialogs import Messagebox
 from pathlib import Path
 from ttkbootstrap.constants import *
+from ttkbootstrap.style import Bootstyle
 from ttkbootstrap.tooltip import ToolTip
 
 PATH = Path(__file__).parent / 'resource'
@@ -32,26 +34,101 @@ class bianjiqi(ttk.Frame):
                 file=PATH / 'cailiao.png'),
         ]
 
-        # 表头
-        top_frame = ttk.Frame(self, padding=5, style=SECONDARY)
-        top_frame.grid(row=0, column=0, columnspan=5, sticky=EW)
+        # 快捷栏
+        kuaijie = ttk.Frame(self, style='primary.TFrame')
+        kuaijie.pack(fill=X, pady=1, side=TOP)
 
-        top_label = ttk.Label(
-            master=top_frame,
-        )
-        top_label.pack(side=LEFT)
+        ## 表头
+        biaotou = ttk.Menu(app)
+        app.config(menu=biaotou)
 
-        # 命令栏
-        mingling_frame = ttk.Frame(self)
-        mingling_frame.grid(row=0, column=0, sticky=EW)
+        ### 文件
+        wenjian = ttk.Menu(biaotou)
+        wenjian.add_command(label='新建')
+        wenjian.add_command(label='打开')
+        wenjian.add_command(label='关闭')
+        wenjian.add_command(label='保存')
+        wenjian.add_command(label='另存为')
+        biaotou.add_cascade(label='文件(F)', menu=wenjian)
 
-        wenjian = ttk.Button(
-            master=mingling_frame,
-            text='文件',
-            style=SECONDARY
-        )
-        wenjian.pack(side=TOP, fill=BOTH,)
 
+        ## 编辑
+        bianji = ttk.Menu(biaotou)
+        bianji.add_command(label='还原')
+        bianji.add_command(label='剪切')
+        bianji.add_command(label='复制')
+        bianji.add_command(label='粘贴')
+        bianji.add_command(label='清除')
+        biaotou.add_cascade(label='编辑(E)',menu=bianji)
+
+        ## 命令
+        mingling = ttk.Menu(biaotou)
+        mingling.add_command(label='结点')
+        mingling.add_command(label='杆件')
+        mingling.add_command(label='约束')
+        mingling.add_command(label='荷载')
+        mingling.add_command(label='材料')
+        biaotou.add_cascade(label='命令(C)', menu=bianji)
+
+        ## 求解
+        qiujie = ttk.Menu(biaotou)
+
+
+        # 工具栏
+        gongju = ttk.Frame(self)
+
+
+
+
+
+        # # 表头
+        # top_frame = ttk.Frame(self, padding=5, style=SECONDARY)
+        # top_frame.grid(row=0, column=0, columnspan=5, sticky=EW)
+        #
+        # top_label = ttk.Label(
+        #     master=top_frame,
+        #     style=SECONDARY
+        # )
+        # top_label.pack(fill=Y, pady=1, side=TOP)
+        #
+        # # 命令栏
+        # mingling_frame = ttk.Frame(self)
+        # mingling_frame.grid(row=0, column=0, sticky=NSEW)
+        #
+        # wenjian = ttk.Button(
+        #     master=mingling_frame,
+        #     text='文件',
+        #     style=(SECONDARY,OUTLINE)
+        # )
+        # wenjian.pack(side=LEFT, fill=BOTH)
+        #
+        # bianji = ttk.Button(
+        #     master=mingling_frame,
+        #     text='编辑',
+        #     style=(DARK,OUTLINE)
+        # )
+        # bianji.pack(side=LEFT, fill=BOTH)
+        #
+        # chakan = ttk.Button(
+        #     master=mingling_frame,
+        #     text='查看',
+        #     style=(SECONDARY,OUTLINE)
+        # )
+        # chakan.pack(side=LEFT, fill=BOTH)
+        #
+        # mingling = ttk.Button(
+        #     master=mingling_frame,
+        #     text='命令',
+        #     style=(SECONDARY, OUTLINE)
+        # )
+        # mingling.pack(side=LEFT, fill=BOTH)
+        #
+        # qiujie = ttk.Button(
+        #     master=mingling_frame,
+        #     text='求解',
+        #     style=(SECONDARY, OUTLINE)
+        # )
+        # qiujie.pack(side=LEFT, fill=BOTH)
 
 
 
